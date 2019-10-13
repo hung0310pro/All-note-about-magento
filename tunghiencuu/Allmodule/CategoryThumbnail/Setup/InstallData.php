5d9fd9e46833122d99445158f1c1ed979da3fb18
Hunglm <hung0210pro@gmail.com>
2019-10-13 22:21:58 +0700

    new module

--- tunghiencuu/Allmodule/CategoryThumbnail/Setup/InstallData.php
+++ tunghiencuu/Allmodule/CategoryThumbnail/Setup/InstallData.php
@@ -43,7 +43,7 @@ class InstallData implements InstallDataInterface
         $categorySetup = $this->categorySetupFactory->create(['setup' => $setup]);
         $categorySetup->addAttribute(
             'catalog_category',
-            'category_thumbnail',
+            'category_thumbnails',
             [
                 'type' => 'varchar',
                 'label' => 'Category Thumbnail Image',
@@ -55,6 +55,39 @@ class InstallData implements InstallDataInterface
                 'group' => 'General Information',
             ]
         );
+
+        $categorySetup->addAttribute(
+                \Magento\Catalog\Model\Category::ENTITY,
+                'is_active_landings',
+                [
+                    'type' => 'int',
+                    'label' => 'Is Landing',
+                    'input' => 'select',
+                    'sort_order' => 10,
+                    'source' => 'Magento\Eav\Model\Entity\Attribute\Source\Boolean',
+                    'required' => false,
+                    'user_defined' => false,
+                    'default' => null,
+                    'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_STORE,
+                    'group' => 'General Information',
+                ]
+            );
+
+            $categorySetup->addAttribute(
+                \Magento\Catalog\Model\Category::ENTITY,
+                'categorytitles',
+                [
+                    'type' => 'varchar',
+                    'label' => 'Category Title',
+                    'input' => 'text',
+                    'sort_order' => 100,
+                    'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_STORE,
+                    'required' => false,
+                    'user_defined' => false,
+                    'default' => null,
+                    'group' => 'General Information',
+                ]
+            );
 
         $setup->endSetup();
     }
